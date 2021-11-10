d77243753882b5785e463760d7766356603320ea
Stepanov Vadim <vadgat777@mail.ru>
2021-11-10 13:01:33 +0700

    Add files via upload

---
+++ Scripts/Reproduction.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reproduction : MonoBehaviour
+{
+	[SerializeField] private GameObject _enemy;
+	[SerializeField] private float _waitTime = 2f;
+	[SerializeField] private float Time = 0f;
+
+	private void Start()
+	{
+		Invoke(nameof(SpawnEnemy), _waitTime);
+	}
+
+	private void SpawnEnemy()
+	{
+		Time++;
+
+		Instantiate(_enemy, transform.position, transform.rotation);
+
+		if (Time >= 10)
+        {
+			return;
+        }
+
+		if (_waitTime > .2f)
+		{
+			Invoke(nameof(SpawnEnemy), _waitTime);
+		}
+
+	}
+}
