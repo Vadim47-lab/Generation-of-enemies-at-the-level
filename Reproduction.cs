20b2050cc8e83c91996922d47cb085dd49e181d2
Stepanov Vadim <vadgat777@mail.ru>
2021-11-15 19:29:07 +0700

    Add files via upload

---
+++ Reproduction.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(GameObject))]
+public class Reproduction : MonoBehaviour
+{
+	[SerializeField] private GameObject _target;
+
+	private void Start()
+	{
+		StartCoroutine(SpawnEnemy());
+	}
+
+	private IEnumerator SpawnEnemy()
+	{
+		var waitForSeconds = new WaitForSeconds(2f);
+
+		while (true)
+		{
+		    Instantiate(_target, transform.position, transform.rotation);
+
+		    yield return waitForSeconds;
+		}
+	}
+}
